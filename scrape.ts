61f947f829bba5f3fdd44130579e83e237190867
Thomas Castiglione <castiglione@mac.com>
2022-02-28 22:36:12 +0800

    remove fake delay

--- scrape.ts
+++ scrape.ts
@@ -2,5 +2,5 @@ import { Datum } from "./Datum";
 import fallbackData from './fallback.json';
 
 export async function getCovidLiveWA(): Promise<Datum[]> {
-    return new Promise(resolve => setTimeout(resolve, 1000, fallbackData.reverse()));
+    return new Promise(resolve => setTimeout(resolve, 0, fallbackData.reverse()));
 }
