026e27bf376304cc1eb2a0460b3bbd51338833c3
Thomas Castiglione <castiglione@mac.com>
2022-02-28 14:05:01 +0800

    render a demo chart

--- app.tsx
+++ app.tsx
@@ -1,5 +1,55 @@
-import React = require('react');
-import * as Server from 'react-dom/server';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {
+    Axis,
+    Grid,
+    LineSeries,
+    XYChart,
+    Tooltip,
+} from '@visx/xychart';
 
-let Greet = () => <h1>Hello, world!</h1>
-console.log(Server.renderToString(<Greet />))
+type Datum = {x: string, y: number};
+
+let data1 = [
+    { x: '2020-01-01', y: 50 },
+    { x: '2020-01-02', y: 10 },
+    { x: '2020-01-03', y: 20 },
+];
+
+let data2 = [
+    { x: '2020-01-01', y: 30 },
+    { x: '2020-01-02', y: 40 },
+    { x: '2020-01-03', y: 80 },
+];
+
+let accessors = {
+    xAccessor: (d: Datum) => d.x,
+    yAccessor: (d: Datum) => d.y,
+};
+
+let Chart = () => (
+    <XYChart height={300} xScale={{ type: 'band' }} yScale={{ type: 'linear' }}>
+      <Axis orientation="bottom" />
+      <Grid columns={false} numTicks={4} />
+      <LineSeries dataKey="Line 1" data={data1} {...accessors} />
+      <LineSeries dataKey="Line 2" data={data2} {...accessors} />
+      <Tooltip<Datum>
+        snapTooltipToDatumX
+        snapTooltipToDatumY
+        showVerticalCrosshair
+        showSeriesGlyphs
+        renderTooltip={({ tooltipData, colorScale }) => (!tooltipData || !colorScale) ? "" : (
+          <div>
+            <div style={{ color: colorScale(tooltipData.nearestDatum!.key) }}>
+              {tooltipData.nearestDatum!.key}
+            </div>
+            {accessors.xAccessor(tooltipData.nearestDatum!.datum)}
+            {', '}
+            {accessors.yAccessor(tooltipData.nearestDatum!.datum)}
+          </div>
+        )}
+      />
+    </XYChart>
+  );
+
+ReactDOM.render(<Chart/>, document.body);
